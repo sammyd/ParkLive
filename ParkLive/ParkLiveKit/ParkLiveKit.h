3d8492ba1348f5b4a0ea267e50ebd2f54c34e5d1
Sam Davies <sam@visualputty.co.uk>
2015-07-12 20:40:22 +0100

    Create utility frameworks

---
+++ ParkLive/ParkLiveKit/ParkLiveKit.h
@@ -0,0 +1,19 @@
+//
+//  ParkLiveKit.h
+//  ParkLiveKit
+//
+//  Created by Sam Davies on 12/07/2015.
+//  Copyright (c) 2015 VisualPutty. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ParkLiveKit.
+FOUNDATION_EXPORT double ParkLiveKitVersionNumber;
+
+//! Project version string for ParkLiveKit.
+FOUNDATION_EXPORT const unsigned char ParkLiveKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ParkLiveKit/PublicHeader.h>
+
+
---
+++ ParkLive/SODAKit/SODAKit.h
@@ -0,0 +1,19 @@
+//
+//  SODAKit.h
+//  SODAKit
+//
+//  Created by Sam Davies on 12/07/2015.
+//  Copyright (c) 2015 VisualPutty. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SODAKit.
+FOUNDATION_EXPORT double SODAKitVersionNumber;
+
+//! Project version string for SODAKit.
+FOUNDATION_EXPORT const unsigned char SODAKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SODAKit/PublicHeader.h>
+
+
