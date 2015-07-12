1c0427be922003e5ffa1f094322d663395abd239
Sam Davies <sam@visualputty.co.uk>
2015-07-12 16:57:25 +0100

    Table controller now works - yay!

---
+++ CarPark.swift
@@ -0,0 +1,67 @@
+//
+//  CarPark.swift
+//  ParkLive
+//
+//  Created by Sam Davies on 12/07/2015.
+//  Copyright © 2015 VisualPutty. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+
+enum CarParkStatus {
+  case Filling
+  case Emptying
+  case Unknown
+}
+
+extension CarParkStatus {
+  init(string: String) {
+    switch string {
+    case "Emptying":
+      self = .Emptying
+    case "Filling":
+      self = .Filling
+    default:
+      self = .Unknown
+    }
+  }
+}
+
+struct CarPark {
+  let status: CarParkStatus
+  let percentage: Int
+  let description: String
+  let name: String
+  let capacity: Int
+  let occupancy: Int
+  let location: CLLocationCoordinate2D
+}
+
+extension CarPark {
+  init?(dict: [String : AnyObject]) {
+    guard let statusStr = dict["status"] as? String,
+      let percentageStr = dict["percentage"] as? String,
+      let percentage = Int(percentageStr),
+      let description = dict["description"] as? String,
+      let name = dict["name"] as? String,
+      let capacityStr = dict["capacity"] as? String,
+      let capacity = Int(capacityStr),
+      let occupancyStr = dict["occupancy"] as? String,
+      let occupancy = Int(occupancyStr),
+      let locationDict = dict["location"] as? [String : AnyObject],
+      let latitudeStr = locationDict["latitude"] as? String,
+      let latitude = Double(latitudeStr),
+      let longitudeStr = locationDict["longitude"] as? String,
+      let longitude = Double(longitudeStr) else {
+        return nil
+    }
+    self.status = CarParkStatus(string: statusStr)
+    self.percentage = percentage
+    self.description = description
+    self.name = name
+    self.capacity = capacity
+    self.occupancy = occupancy
+    self.location = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+  }
+}
--- ParkLive WatchKit Extension/InterfaceController.swift
+++ ParkLive WatchKit Extension/InterfaceController.swift
@@ -8,15 +8,36 @@
 
 import WatchKit
 import Foundation
-import SODAKit
 
 class InterfaceController: WKInterfaceController {
+  
+  @IBOutlet var carparkTable: WKInterfaceTable!
+  
+  var carparks : [CarPark]? {
+    didSet {
+      guard let carparks = carparks else { return }
+      configureTableWithData(carparks)
+    }
+  }
   
   override func awakeWithContext(context: AnyObject?) {
     super.awakeWithContext(context)
     
     // Configure interface objects here.
-    
+    let client = SODAClient(domain: "data.bathhacked.org", token: "d1TbPXUzf0zqTuCy6MDCTtJR6")
+    let query = client.queryDataset("u3w2-9yme")
+    query.orderDescending("percentage").get {
+      results in
+      switch(results) {
+      case .Dataset(let dataset):
+        let carparks = dataset.map { CarPark(dict: $0) }
+                              .filter { $0 != nil }
+                              .map { $0! }
+        self.carparks = carparks
+      case .Error(let error):
+        print(error)
+      }
+    }
   }
   
   override func willActivate() {
@@ -27,6 +48,17 @@ class InterfaceController: WKInterfaceController {
   override func didDeactivate() {
     // This method is called when watch view controller is no longer visible
     super.didDeactivate()
+  }
+  
+  
+  private func configureTableWithData(data: [CarPark]) {
+    carparkTable.setNumberOfRows(data.count, withRowType: "CarParkRow")
+    for i in 0..<data.count {
+      if let row = carparkTable.rowControllerAtIndex(i) as? CarParkRow {
+        let carpark = data[i]
+        row.carpark = carpark
+      }
+    }
   }
   
 }
